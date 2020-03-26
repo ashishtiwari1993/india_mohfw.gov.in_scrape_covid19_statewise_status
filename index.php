7793c3bc33e7e7b41b09aef4e4881c3288a64346
Ashish Tiwari <atashish171@gmail.com>
2020-03-27 00:10:02 +0530

    added some basic validation

--- index.php
+++ index.php
@@ -6,29 +6,49 @@ $html = file_get_html('https://www.mohfw.gov.in/');
 
 $result = [];
 
-$head = ['s_no','state','total_confirmed_case_indian_national','total_confirmed_cases_foreign_national','cured_discharge_migrated','death'];
+$paramValueAllowed = ['state'];
 
-foreach($html->find('div#cases') as $e){
-		$i = 1;
-		foreach($e->find('div.table-responsive tbody tr') as $trk){
-				$stateWiseData = [];
-				foreach($trk->find('td') as $tdk){
-						$stateWiseData[] = $tdk->innertext;
-				}
+$q = null;
 
-				$data = [];
-				foreach($stateWiseData as $stkey => $stvalue){
-					$data[$head[$stkey]] = $stvalue;
-				}
-
-				$result[] = $data;
-
-		$i++;	
-		}	
+if(array_key_exists('q',$_GET)){
+	$q = $_GET['q'];
 }
 
-$totalElement = count($result);
-unset($result[$totalElement - 1]);
-unset($result[$totalElement - 2]);
+if(!empty($q)){
 
-echo json_encode($result,JSON_PRETTY_PRINT);
+	if(in_array($q,$paramValueAllowed)){
+
+	if($q=="state"){
+				$head = ['s_no','state','total_confirmed_case_indian_national','total_confirmed_cases_foreign_national','cured_discharge_migrated','death'];
+				foreach($html->find('div#cases') as $e){
+								$i = 1;
+								foreach($e->find('div.table-responsive tbody tr') as $trk){
+												$stateWiseData = [];
+												foreach($trk->find('td') as $tdk){
+																$stateWiseData[] = $tdk->innertext;
+												}
+
+												$data = [];
+												foreach($stateWiseData as $stkey => $stvalue){
+																$data[$head[$stkey]] = $stvalue;
+												}
+
+												$result[] = $data;
+
+												$i++;	
+								}	
+				}
+
+				$totalElement = count($result);
+				unset($result[$totalElement - 1]);
+				unset($result[$totalElement - 2]);
+
+				echo json_encode($result,JSON_PRETTY_PRINT);
+	}
+
+	}else{
+		echo json_encode(array('status' => 'fail','message' => "Please provide the valid value for parameter 'q'."),JSON_PRETTY_PRINT);
+	}
+}else{
+	echo json_encode(array('status' => 'fail','message' => "Please provide the 'q' parameter."),JSON_PRETTY_PRINT);
+}
